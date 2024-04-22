f3fe1a41c9c9d9e7044066393e3e586adb5653bc
Cícero Mello <ciceromello.dev@gmail.com>
2024-04-22 19:25:08 -0300

    🚑 - Provisional fix for deployment, kkkk

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,26 +1,28 @@
-import React, { FunctionComponent, useLayoutEffect, useMemo } from "react"
+import React, { FunctionComponent, useEffect, useLayoutEffect, useMemo, useState } from "react"
 import { PageProps, graphql } from "gatsby"
 import { Banner } from "../../components"
 import { HomeProps, QueryGatsbyImages } from "./type"
 import { getRandomMediaToBanner } from "./core"
 import * as Styled from "./styles"
+import { BannerMedia } from "../../components/banner/types"
 
 export const Home: FunctionComponent<HomeProps> = ({ data }) => {
-    const bannerGatsbyImages: QueryGatsbyImages[] = data ? data.banner.edges.map(
-        (obj: { node: any }) => ({...obj.node})
-    ) : []
-    const posterGatsbyImages: QueryGatsbyImages[] = data ? data.poster.edges.map(
-        (obj: { node: any }) => ({...obj.node})
-    ) : []
-    const bannerMediaList = getRandomMediaToBanner(bannerGatsbyImages)
+    const [bannerMediaList, setBannterMediaList] = useState<BannerMedia[]>([])
 
-    // useLayoutEffect(() => {
-    //     getRandomMediaToBanner(bannerGatsbyImages)
-    // }, [])
+    useLayoutEffect(() => {
+        const bannerGatsbyImages: QueryGatsbyImages[] = data ? data.banner.edges.map(
+            (obj: { node: any }) => ({...obj.node})
+        ) : []
+        const posterGatsbyImages: QueryGatsbyImages[] = data ? data.poster.edges.map(
+            (obj: { node: any }) => ({...obj.node})
+        ) : []
+
+        if(bannerGatsbyImages.length > 0) setBannterMediaList(getRandomMediaToBanner(bannerGatsbyImages))
+    }, [data])
 
     return (
         <Styled.Home>
-            <Banner media={bannerMediaList[0]}/>
+            {bannerMediaList.length> 0 && <Banner media={bannerMediaList[0]}/>}
         </Styled.Home>
     )
 }
