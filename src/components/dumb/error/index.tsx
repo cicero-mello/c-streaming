e4d383401b110824548a8d64c86153d128a18794
Cícero Mello <ciceromello.dev@gmail.com>
2024-09-21 17:44:07 -0300

    ♿️ Improving accessibility on error component

--- src/components/dumb/error/index.tsx
+++ src/components/dumb/error/index.tsx
@@ -6,7 +6,12 @@ export const Error: FC<ErrorProps> = ({
     errorCode, ...rest
 }) => (
     <S.Component {...rest}>
-        <S.ErrorCodeMessage $errorCode={errorCode}>
+        <S.ErrorCodeMessage
+            $errorCode={errorCode}
+            aria-label={`ERROR: ErrorCode: ${errorCode}`}
+            role="presentation"
+            tabIndex={0}
+        >
             {errorCode}
         </S.ErrorCodeMessage>
     </S.Component>
