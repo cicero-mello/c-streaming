2c6cf6baf2d64303849fdc64331f3b299ea61142
Cícero Mello <ciceromello.dev@gmail.com>
2024-10-02 00:50:15 -0300

    📝 Adding documentation about use-aria-notification

--- src/hooks/use-aria-notification/index.tsx
+++ src/hooks/use-aria-notification/index.tsx
@@ -8,6 +8,22 @@ const AriaNotificationContext = createContext<UseAriaNotification>({
     clearAriaNotification: () => {}
 })
 
+/**
+ * Returns a object with
+ * a function to trigger the read of screen readers.
+ *
+ * When you call this function:
+ *
+ * 1 - A hidden tag will be created and rendered, with aria-live:polite
+ *
+ * 2 - The message passed in the function will be inserted in aria-label
+ *
+ * 3 - The screen reader will read the message
+ *
+ * 4 - After some user action, or some time, the hidden tag will be removed from HTML
+ *
+ * @function
+ */
 export const useAriaNotification = () => {
     const context = useContext(AriaNotificationContext)
 
