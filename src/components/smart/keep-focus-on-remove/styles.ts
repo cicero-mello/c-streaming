b4ab95fe49053aa7c8da27978a89e57a03fbaf6d
Cícero Mello <ciceromello.dev@gmail.com>
2024-10-01 22:20:36 -0300

    🩹 Fixing behavior about auto scroll top when HiddenTag receive focus

--- src/components/smart/keep-focus-on-remove/styles.ts
+++ src/components/smart/keep-focus-on-remove/styles.ts
@@ -4,11 +4,11 @@ export const HiddenTag = styled.p.attrs({
     className: "hidden-tag-keep-focus-on-remove",
     role: "presentation"
 })`
-    position: absolute;
     width: 0;
     height: 0;
     opacity: 0;
     overflow: hidden;
+    z-index: -1;
 
     &:focus-visible{
         outline: none;
